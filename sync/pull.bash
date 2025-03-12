be74049f7a8ccb4519e873cdf1600866dc4e4d6b
codenie <niebuqing@sjtu.edu.cn>
2025-03-12 13:26:56 +0800

    修改同步脚本

--- sync/pull.bash
+++ sync/pull.bash
@@ -1,3 +1,7 @@
-echo "pull from server 2..."
+# echo "pull from server 2..."
+# # rsync -av --include-from="sync/pull_include.txt" --exclude="/*" c2:~/research/humanoid_symmetry/g1-ppo/ ./
+# rsync -av c2:~/research/humanoid_symmetry/g1-ppo/legged_gym/logs/ ./legged_gym/logs/
+
+echo "pull from cz server 2..."
 # rsync -av --include-from="sync/pull_include.txt" --exclude="/*" c2:~/research/humanoid_symmetry/g1-ppo/ ./
-rsync -av c2:~/research/humanoid_symmetry/g1-ppo/legged_gym/logs/ ./legged_gym/logs/
+rsync -av cz_s2:~/research/humanoid_symmetry/g1-ppo-symm/legged_gym/logs/ ./legged_gym/logs/
--- sync/push.bash
+++ sync/push.bash
@@ -1,2 +1,5 @@
-echo "push to server 2..."
-rsync -av --exclude-from="sync/push_exclude.txt" ./ c2:~/research/humanoid_symmetry/g1-ppo/
+# echo "push to server 2..."
+# rsync -av --exclude-from="sync/push_exclude.txt" ./ c2:~/research/humanoid_symmetry/g1-ppo/
+
+echo "push to cz server 2..."
+rsync -av --exclude-from="sync/push_exclude.txt" ./ cz_s2:~/research/humanoid_symmetry/g1-ppo-symm/
